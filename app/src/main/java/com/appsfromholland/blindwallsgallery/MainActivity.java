2fc84037dab505a681a9b882afd4c5a49fee588b
Diederich Kroeske <dkroeske@popd.nl>
2019-05-02 09:36:58 +0200

    Fixed UI

--- app/src/main/java/com/appsfromholland/blindwallsgallery/MainActivity.java
+++ app/src/main/java/com/appsfromholland/blindwallsgallery/MainActivity.java
@@ -25,10 +25,16 @@ public class MainActivity extends AppCompatActivity {
                 "Dummy Description",
                 "https://camo.githubusercontent.com/fe9976caf90267e2f565db30b24d2f3076bee52a/68747470733a2f2f74352e72627863646e2e636f6d2f6164613134666132356431363066333161643962656334313964656332643965");
         murals.add(mural);
+        murals.add(mural);
+        murals.add(mural);
+        murals.add(mural);
+        murals.add(mural);
+        murals.add(mural);
+
 
         recyclerView = findViewById(R.id.blindwallsRecyclerViewId);
         recyclerView.setLayoutManager(new GridLayoutManager(
-                this, 4, GridLayoutManager.VERTICAL, false)
+                this, 2, GridLayoutManager.VERTICAL, false)
         );
         blindwallsAdapter = new BlindwallsAdapter(murals);
         recyclerView.setAdapter(blindwallsAdapter);
